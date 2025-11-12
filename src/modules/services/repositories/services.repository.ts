9089cdc825a0075f6bf823b8602a853d0936e280
Patrick Oduro <oduro.kwabena.patrick@gmail.com>
2025-11-12 17:49:35 +0000

    fix get service category bugs

--- src/modules/services/repositories/services.repository.ts
+++ src/modules/services/repositories/services.repository.ts
@@ -1,18 +1,22 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Service } from '../schemas/service.schema';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import mongoose, { Model, Mongoose } from "mongoose";
+import { Service } from "../schemas/service.schema";
 
 @Injectable()
 export class ServicesRepository {
-  constructor(@InjectModel(Service.name) private serviceModel: Model<Service>) {}
+  constructor(
+    @InjectModel(Service.name) private serviceModel: Model<Service>
+  ) {}
 
   async findAll(): Promise<Service[]> {
     return this.serviceModel.find({ isActive: true }).exec();
   }
 
   async findById(id: string): Promise<Service | null> {
-    return this.serviceModel.findOne({ id, isActive: true }).exec();
+    return this.serviceModel
+      .findOne({ _id: new mongoose.Types.ObjectId(id), isActive: true })
+      .exec();
   }
 
   async create(serviceData: Partial<Service>): Promise<Service> {
